a02f0e3ed39e70e8ef6cec85d9d722f41bab477d
flashdrag <flashdrag@gmail.com>
2023-03-30 12:22:31 +0100

    Refactor: simplify js code and remove jQuery

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,36 @@
 const API_KEY = ""
 const API_URL = "https://ci-jshint.herokuapp.com/api"
-/*
-// triggering Bootstrap modals using JavaScript
-$('#status-btn').click(function () {
-  let resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'))
-  resultsModal.show()
-})
-// triggering Bootstrap modals using JavaScript or jQuery
-$('#status-btn').click(function () {
-  $('#resultsModal').modal('show');
-})
-*/
 
-$('#status-btn').click(displayStatus)
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 
-async function displayStatus() {
-  try {
-    const status = await getStatus();
-    let heading = 'API Key Status';
-    let strStatus = `<p>${status.msg}</p><p>${status.date}</p>`;
+document.getElementById("status-btn").addEventListener("click", getStatus);
 
-    $('#resultsModalTitle').text(heading);
-    $('#results-content').html(strStatus);
-    $('#resultsModal').modal('show');
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 async function getStatus() {
+
   const queryString = `${API_URL}?api_key=${API_KEY}`;
+
   const response = await fetch(queryString);
+
   const data = await response.json();
-  if (!response.ok) {
-    throw new Error(data.error);
+
+  if (response.ok) {
+      displayStatus(data);
+  } else {
+      throw new Error(data.error);
   }
 
-  const [day, month, year] = data.expiry.split('-').map(Number);
-  const expiryDate = new Date(year, month - 1, day);
-  const isValid = expiryDate >= new Date();
-  let msg = isValid ? 'Your key is valid until' :
-    'The service is no longer available!\nThe API key is expired by';
+}
 
-  return { msg: msg, date: expiryDate.toLocaleDateString('tr-TR') };
+function displayStatus(data) {
+
+  let heading = "API Key Status";
+  let results = `<div>Your key is valid until</div>`;
+  results += `<div class="key-status">${data.expiry}</div>`;
+
+  document.getElementById("resultsModalTitle").innerText = heading;
+  document.getElementById("results-content").innerHTML = results;
+  resultsModal.show();
+
 }
